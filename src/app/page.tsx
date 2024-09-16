1bd5f801b4c6d561914282fb50eaf2792177b6ba
lenayoo <lenayoo17@gmail.com>
2024-09-16 17:23:09 +0900

    add random quote function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,25 +7,36 @@ import { mockQuotes } from '@/mock/quotes';
 import type { Quotes } from '@/types';
 
 export default function Home() {
+  const [oneQuote, setOneQuote] = useState<Quotes | undefined>(undefined);
   const [quotes, setQuotes] = useState<Quotes[] | undefined>(undefined);
 
   useEffect(() => {
     setQuotes(mockQuotes);
-  }, []);
+    if (quotes) {
+      setOneQuote(quotes[randomQuote]);
+    }
 
-  console.log(quotes);
+    console.log(oneQuote);
+  }, [quotes]);
+
+  const length = quotes?.length;
+  const randomQuote = getRandomInt(length!);
+
+  function getRandomInt(max: number) {
+    return Math.floor(Math.random() * max);
+  }
 
   return (
     <div className={styles.page}>
-      hello lena
-      {quotes
-        ? quotes.map((quote) => (
-            <>
-              <div>{quote.quote}</div>
-              <div>{quote.author}</div>
-            </>
-          ))
-        : ''}
+      <h3>Today's inspirational quote⛵️</h3>
+      {oneQuote ? (
+        <div className={styles.container} key={oneQuote.tags.length}>
+          <div className={styles.quoteBox}>{oneQuote.quote}</div>
+          <div className={styles.autherBox}>{oneQuote.author}</div>
+        </div>
+      ) : (
+        '오늘의 문장을 불러오는데 실패했습니다.'
+      )}
     </div>
   );
 }
